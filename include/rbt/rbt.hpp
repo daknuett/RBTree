ac7fe04e1b418b026895c7edef31ab4663699fd7
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2021-09-10 11:53:51 +0200

    merged fixes for tree iteration

--- include/rbt/rbt.hpp
+++ include/rbt/rbt.hpp
@@ -15,17 +15,18 @@ namespace rbt
         Node * m_lower;
         Node * m_higher;
         int m_value;
-        char m_marker;
         inline bool is_lower_child(void);
         inline bool has_uncle(void);
         inline bool has_red_child(void);
+        //
         // WARNING: This method does NOT check for NULL pointers!
         inline Node * get_uncle(void);
+
         void recursive_inorder_export(std::vector<int> & vect);
         int check_rbt_pathlength(void);
-        void repair_markers(char marker);
+        Node * get_leftmost(void);
         public:
-        Node(Node * parent, int value, char marker);
+        Node(Node * parent, int value);
         Node(Node & orig);
         Node(Node * parent, Node * orig);
         void recursively_delete(void);
@@ -41,15 +42,12 @@ namespace rbt
     {
         private:
         Node * m_root;
-        char m_marker_mask;
-        char m_marker_sanity;
         size_t m_count;
         Node * do_insert(int value);
         inline void delete_this_node(Node * c_node);
         void repair_after_insert(Node * causing_node);
         inline void left_rotate(Node * B);
         inline void right_rotate(Node * B);
-        inline void repair_markers_if_needed(void);
         public:
         void insert(int value);
         void delete_value(int value);
@@ -68,7 +66,6 @@ namespace rbt
             private:
             RBTree * m_tree;
             Node * m_cnode;
-            bool m_finished;
             public:
             RBTIterator(RBTree * tree, Node * cnode);
             RBTIterator operator++(int);
